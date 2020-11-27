2a69ba0b9ea07fa8d73d6cd3a1f6414ab40f98d0
Guilherme Sigoli <gui.sigoli@hotmail.com>
2020-11-26 21:11:44 -0300

    add: string calculation

--- app/src/main/java/com/ilogis/calculator/MainActivity.java
+++ app/src/main/java/com/ilogis/calculator/MainActivity.java
@@ -1,4 +1,5 @@
 package com.ilogis.calculator;
+import org.mariuszgromada.math.mxparser.*;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,39 +25,78 @@ public class MainActivity extends AppCompatActivity {
     public void setTextOnView(View v) {
         switch (v.getId()) {
             case R.id.zero_bt:
-                viewfinder.setText(viewfinder.getText() + "0"); //R.string.zero;
+                viewfinder.setText(viewfinder.getText() + "0");
                 break;
             case R.id.one_bt:
-                viewfinder.setText(viewfinder.getText() + "1"); //R.string.zero;
+                viewfinder.setText(viewfinder.getText() + "1");
                 break;
             case R.id.two_bt:
-                viewfinder.setText(viewfinder.getText() + "2"); //R.string.zero;
+                viewfinder.setText(viewfinder.getText() + "2");
                 break;
             case R.id.three_bt:
-                viewfinder.setText(viewfinder.getText() + "3"); //R.string.zero;
+                viewfinder.setText(viewfinder.getText() + "3");
                 break;
             case R.id.four_bt:
-                viewfinder.setText(viewfinder.getText() + "4"); //R.string.zero;
+                viewfinder.setText(viewfinder.getText() + "4");
                 break;
             case R.id.five_bt:
-                viewfinder.setText(viewfinder.getText() + "5"); //R.string.zero;
+                viewfinder.setText(viewfinder.getText() + "5");
                 break;
             case R.id.six_bt:
-                viewfinder.setText(viewfinder.getText() + "6"); //R.string.zero;
+                viewfinder.setText(viewfinder.getText() + "6");
                 break;
             case R.id.seven_bt:
-                viewfinder.setText(viewfinder.getText() + "7"); //R.string.zero;
+                viewfinder.setText(viewfinder.getText() + "7");
                 break;
             case R.id.eight_bt:
-                viewfinder.setText(viewfinder.getText() + "8"); //R.string.zero;
+                viewfinder.setText(viewfinder.getText() + "8");
                 break;
             case R.id.nine_bt:
-                viewfinder.setText(viewfinder.getText() + "9"); //R.string.zero;
+                viewfinder.setText(viewfinder.getText() + "9");
                 break;
-            case R.id.clean_bt:
-                viewfinder.setText("");//R.string.zero;
+            case R.id.sum_bt:
+                viewfinder.setText(viewfinder.getText() + "+");
+                break;
+            case R.id.sub_bt:
+                viewfinder.setText(viewfinder.getText() + "-");
+                break;
+            case R.id.mult_bt:
+                viewfinder.setText(viewfinder.getText() + "x");
+                break;
+            case R.id.div_bt:
+                viewfinder.setText(viewfinder.getText() + "/");
+                break;
+            case R.id.dot_bt:
+                viewfinder.setText(viewfinder.getText() + ".");
+                break;
+            case R.id.backspace_bt:
+                viewfinder.setText(backspace(viewfinder.getText().toString()));
+                break;
+            case R.id.calc_bt:
+                viewfinder.setText(calc(viewfinder.getText().toString()));
+                break;
+            case R.id.clear_bt:
+                viewfinder.setText("");
                 break;
         }
+    }
+
+    public String backspace(String str) {
+        if (str != null && str.length() > 0) {
+            str = str.substring(0, str.length() - 1);
+        }
+        return str;
+    }
+
+    public String parseToMathExpression(String str) {
+        return str.replaceAll("x","*");
+    }
+
+    public String calc(String str) {
+        System.out.println(str);
+        Expression e = new Expression(parseToMathExpression(str));
+        Double result = e.calculate();
+        return result.toString();
     }
 
     @Override
